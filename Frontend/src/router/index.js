a7504e048cee6b5e9ca8599b26f9ea644d4390da
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-05-11 15:42:38 +0900

    タイトルの変更

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -6,19 +6,30 @@ const router = createRouter({
     {
       path: '/:floor?/:id?',
       name: 'map',
-      component: () => import('@/views/Map.vue')
+      component: () => import('@/views/Map.vue'),
+      meta: { title: '西高Map', desc: 'マップ' }
     },
     {
       path: '/guide',
       name: 'guide',
-      component: () => import('@/views/Guide.vue')
+      component: () => import('@/views/Guide.vue'),
+      meta: { title: '使い方 |西高Map', desc: '使い方' }
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('@/views/About.vue')
+      component: () => import('@/views/About.vue'),
+      meta: { title: 'このサイトについて |西高Map', desc: 'このサイトについて' }
     }
   ]
+})
+
+// titleを後から変更する
+router.afterEach((to) => {
+  const TITLE = to.meta.title
+  const DESC = to.meta.desc
+  document.title = TITLE
+  document.querySelector("meta[name='description']").setAttribute('content', DESC)
 })
 
 export default router
