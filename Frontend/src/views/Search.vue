4a79642d989d5a7ad2bae01fbe1204bc4b87d696
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-05-13 00:23:53 +0900

    検索アルゴリズムの改善

--- Frontend/src/views/Search.vue
+++ Frontend/src/views/Search.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="background" id="search">
+    <div class="background text" id="search">
         <p class="textTitle">マップ検索</p>
-        <form class="searchBox" @submit.prevent="doSearch()">
-            <input type="text" class="searchInput" placeholder="検索" v-model="searchWord" required>
-            <button class="searchButton">
-                <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-            </button>
-        </form>
-        <ul>
-            <li v-for="place, key in PlaceInfoList" :key="key" @click="move(place.floor, place.id)" class="place">
+        <div class="searchBox">
+            <input id="searchInput" type="text" class="searchInput" placeholder="検索" @input="doSearch()"
+                v-model="searchWord" required>
+            <label for="searchInput"><font-awesome-icon :icon="['fas', 'magnifying-glass']" /></label>
+            <font-awesome-icon @click="reserSearch()" :icon="['fas', 'xmark']" />
+        </div>
+        <p>スペースで区切ることでOR検索が可能<br>
+            {文字列} → {文字列}を全場所から検索<br>
+            name:{文字列} → 名前に{文字列}を含む場所を検索<br>
+            floor:{文字列} → 階に{文字列}を含む場所を検索<br>
+            place:{文字列} → 場所に{文字列}を含む場所を検索<br>
+            desc:{文字列} → 説明に{文字列}を含む場所を検索</p>
+        <div v-for="place, key in PlaceInfoList" :key="key" @click="move(place.floor, place.id)">
+            <div v-if="place.isShow" class="place">
                 <p class="name">{{ place.name }}</p>
                 <p>
-                    <span class="position" v-if="place.floor != null">
+                    <span class="position" v-if="place.floorName != null">
                         <font-awesome-icon :icon="['fas', 'stairs']" />
-                        {{ place.floor }}
+                        {{ place.floorName }}
                     </span>
                     <span class="position" v-if="place.place != null">
                         <font-awesome-icon id="locationIcon" :icon="['fas', 'location-dot']" />
@@ -21,8 +27,8 @@
                     </span>
                 </p>
                 <p v-html="place.desc" v-if="place.desc != null"></p>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
@@ -41,14 +47,15 @@ for (let floorId = 0; floorId < PlaceInfo.length; floorId++) {
             PlaceInfoList.push({
                 id: PlaceInfoKeys[idNum],
                 floor: floorId,
+                floorName: PlaceInfo[floorId].__FloorDisplayName__,
                 name: PlaceInfo[floorId][PlaceInfoKeys[idNum]].name,
                 desc: PlaceInfo[floorId][PlaceInfoKeys[idNum]].desc,
-                place: PlaceInfo[floorId][PlaceInfoKeys[idNum]].place
+                place: PlaceInfo[floorId][PlaceInfoKeys[idNum]].place,
+                isShow: true
             })
         }
     }
 }
-console.log(PlaceInfoList);
 
 function move(floor, id) {
     router.push(`${floor}/${id}`)
@@ -57,24 +64,94 @@ function move(floor, id) {
 
 const searchWord = ref('')
 
-function doSearch() {
-    console.log(searchWord.value);
+
+function reserSearch() {
+    searchWord.value = ''
+    doSearch()
 }
 
+function doSearch() {
+    // 検索ワードを処理しやすい形式に変換
+    // 小文字で検索するために全て小文字に変換
+    const searchList = searchWord.value.split(' ')
+    for (let i = 0; i < searchList.length; i++) {
+        if (searchList[i].split(":", 2).length == 2) {
+            searchList[i] = searchList[i].split(":", 2)
+        } else {
+            searchList[i] = ["normal", searchList[i]]
+        }
+        searchList[i][1] = searchList[i][1].toLowerCase()
+    }
+    console.log(searchList);
+    // 一旦全て非表示にする
+    for (let i = 0; i < PlaceInfoList.length; i++) {
+        PlaceInfoList[i].isShow = false
+    }
+    for (let i = 0; i < searchList.length; i++) {
+        switch (searchList[i][0]) {
+            case "normal":
+                for (let j = 0; j < PlaceInfoList.length; j++) {
+                    if ((PlaceInfoList[j].name != null && PlaceInfoList[j].name.toLowerCase().includes(searchList[i][1])) ||
+                        (PlaceInfoList[j].floorName != null && PlaceInfoList[j].floorName.toLowerCase().includes(searchList[i][1])) ||
+                        (PlaceInfoList[j].place != null && PlaceInfoList[j].place.toLowerCase().includes(searchList[i][1])) ||
+                        (PlaceInfoList[j].desc != null && PlaceInfoList[j].desc.toLowerCase().includes(searchList[i][1]))) {
+                        PlaceInfoList[j].isShow = true
+                    }
+                    console.log(searchList[i][1]);
+                }
+                break;
+            case "name":
+                for (let j = 0; j < PlaceInfoList.length; j++) {
+                    if (PlaceInfoList[j].name != null && PlaceInfoList[j].name.toLowerCase().includes(searchList[i][1])) {
+                        PlaceInfoList[j].isShow = true
+                    }
+                }
+                break;
+            case "floor":
+                for (let j = 0; j < PlaceInfoList.length; j++) {
+                    if (PlaceInfoList[j].floorName != null && PlaceInfoList[j].floorName.toLowerCase().includes(searchList[i][1])) {
+                        PlaceInfoList[j].isShow = true
+                    }
+                }
+                break;
+            case "place":
+                for (let j = 0; j < PlaceInfoList.length; j++) {
+                    if (PlaceInfoList[j].place != null && PlaceInfoList[j].place.toLowerCase().includes(searchList[i][1])) {
+                        PlaceInfoList[j].isShow = true
+                    }
+                }
+                break;
+            case "desc":
+                for (let j = 0; j < PlaceInfoList.length; j++) {
+                    if (PlaceInfoList[j].desc != null && PlaceInfoList[j].desc.toLowerCase().includes(searchList[i][1])) {
+                        PlaceInfoList[j].isShow = true
+                    }
+                }
+                break;
+        }
+    }
+}
 
 </script>
 <style scoped>
-#search {
-    overflow: scroll;
+p {
+    color: var(--MainBodyColor);
 }
 
 .place {
-    border: 1px solid black;
+    border: 1px solid var(--MainBodyColor);
     margin: 5px;
     padding: 5px;
+    border-radius: 10px;
+    cursor: pointer;
 }
 
 .position {
     margin: 0 5px 0 5px;
+}
+
+.name {
+    font-size: 20px;
+    font-weight: bold;
 }
 </style>
