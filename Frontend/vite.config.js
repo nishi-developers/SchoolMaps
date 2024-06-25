f03d8e51ff8956fa33700b3cc69d808318dbc111
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-06-25 03:56:53 +0000

    manifest.jsonをまで作成

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,101 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: '西高Map',
+        theme_color: '#bee0ff',
+        background_color: '#f3f9ff',
+        display: 'standalone',
+        start_url: './index.html',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '256x256'
+          },
+          {
+            src: 'img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/apple-touch-icon-60x60.png',
+            sizes: '60x60',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/apple-touch-icon-76x76.png',
+            sizes: '76x76',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/apple-touch-icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/apple-touch-icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/msapplication-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/safari-pinned-tab.svg',
+            sizes: '11x16'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
