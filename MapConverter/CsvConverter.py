ff56ae1124a6658633a57b97ecfeac9fc0cf3026
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-11-15 10:22:49 +0900

    コンバーターで任意の数のimageも変換できるように

--- MapConverter/CsvConverter.py
+++ MapConverter/CsvConverter.py
@@ -19,11 +19,15 @@ with open(InputFilePath, "r", encoding="utf-8") as f:
 Converted = {}
 # 最初の行を削除
 for i in range(1, len(PlaceInfo) - 1):
+    images = []
+    for j in range(len(PlaceInfo[i]) - 5):
+        images.append(PlaceInfo[i][5+j])
     Converted[PlaceInfo[i][0]] = {
         "floor": int(PlaceInfo[i][1]),
         "name": PlaceInfo[i][2],
         "words": PlaceInfo[i][3],
         "desc": PlaceInfo[i][4],
+        "images": images
     }
 
 # ファイルに書き出し
