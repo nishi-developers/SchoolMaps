4d19c411bc368af956cbb847fa4dc66512f22e33
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-11-17 04:30:34 +0000

    ガイドの調整

--- Frontend/src/views/Guide.vue
+++ Frontend/src/views/Guide.vue
@@ -3,30 +3,30 @@
         <p class="textTitle">使い方</p>
         <p class="textH">Map (PC・マウス)</p>
         <ul class="textP">
-            <li>ドラッグで移動</li>
-            <li>ホイールでズームイン・アウト</li>
-            <li>ホイールを押して左右に動かすことで回転</li>
+            <li>- ドラッグで移動</li>
+            <li>- ホイールでズームイン・アウト</li>
+            <li>- ホイールを押して左右に動かすことで回転</li>
         </ul>
         <p class="textH">Map (スマホ・タッチ)</p>
         <ul class="textP">
-            <li>スワイプで移動</li>
-            <li>ピンチイン・アウトでズームイン・アウト</li>
-            <li>2本指で回転させて回転</li>
+            <li>- スワイプで移動</li>
+            <li>- ピンチイン・アウトでズームイン・アウト</li>
+            <li>- 2本指で回転させて回転</li>
         </ul>
         <p class="textH">詳細画面</p>
         <ul class="textP">
-            <li>バーをスワイプで閉じる・拡大・縮小</li>
-            <li>バーをタップで閉じる </li>
+            <li>- バーをスワイプで閉じる・拡大・縮小</li>
+            <li>- バーをタップで閉じる </li>
         </ul>
         <p class="textH">検索画面</p>
         <ul class="textP">
-            <li>半角/全角スペースで区切ることでOR検索が可能</li>
+            <li>- 半角/全角スペースで区切ることでOR検索が可能</li>
         </ul>
     </div>
 </template>
 <style scoped>
 ul {
-    padding-left: 20px;
+    padding-left: 10px;
 }
 
 #search {
