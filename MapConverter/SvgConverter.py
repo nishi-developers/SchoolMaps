aec6cdb6fa3e7ceaa0563b7512e442e284821a18
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2024-11-28 21:02:35 +0900

    コンバーターの追加

--- MapConverter/SvgConverter.py
+++ MapConverter/SvgConverter.py
@@ -10,21 +10,9 @@ PlaceInfoLabels = []
 
 # ファイルのパスを取得
 
-InputFilePath = input("InputFilePath(svg): ")
-if InputFilePath == "":
-    InputFilePath = "map.svg"
-
-TemplateFilePath = input("TemplateFilePath(vue): ")
-if TemplateFilePath == "":
-    TemplateFilePath = "template.vue"
-
-OutputFilePath = input("OutputFilePath(vue): ")
-if OutputFilePath == "":
-    OutputFilePath = "out/map-output.vue"
-
-# FloorInfoFilePath = input("FloorInfoFilePath(json): ")
-# if FloorInfoFilePath == "":
-#     FloorInfoFilePath = "out/FloorInfo.json"
+InputFilePath = "in-out/input.svg"
+OutputFilePath = "in-out/output.vue"
+TemplateFilePath = "template.vue"
 
 # SVGファイルを読み込み
 
@@ -48,16 +36,6 @@ for child in root:
     # "-"以下はid重複防止用なので削除
     if id != None:
         placeid = id.split("-")[0]
-        # placeの場合
-        if (
-            placeid != None
-            and placeid != "none"
-            and placeid != "base"
-            and placeid != "label"
-        ):
-            # PlaceInfoLabelsに追加
-            if placeid not in PlaceInfoLabels:
-                PlaceInfoLabels.append(placeid)
 
 # 文字列に変換
 file = ET.tostring(root, encoding="utf-8", xml_declaration=False).decode("utf-8")
@@ -73,22 +51,3 @@ with open(OutputFilePath, mode="w", encoding="utf-8") as f:
             "<template></template>", "<template>\n" + file + "\n</template>"
         )
     )
-
-# PlaceInfoの作成
-# PlaceInfo = """
-#     {
-#         "__FloorName__": "",
-#         "__FloorDisplayName__": "",
-#         "__MapSizeWidth__": 0,
-#         "__MapSizeHeight__": 0,
-#     """
-# for i in PlaceInfoLabels:
-#     PlaceInfo += '"' + i + '": {"name": "' + i + '", "description": ""},\n'
-# # 最後の,を削除
-# PlaceInfo = PlaceInfo[:-2]
-# PlaceInfo += "}"
-
-
-# # ファイルに書き出し
-# with open(FloorInfoFilePath, mode="w", encoding="utf-8") as f:
-#     f.write(PlaceInfo)
