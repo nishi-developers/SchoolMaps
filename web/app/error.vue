13773b5b0332f5be5db21d3d3f6aad3efa505ca1
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-07 00:53:47 +0900

    エラーページの404ステータスコード処理を改善

--- web/app/error.vue
+++ web/app/error.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="background text widthLimit">
-    <p class="textTitle">404 Not Found</p>
-    <p class="textH">お探しのページは見つかりませんでした。</p>
-    <p class="textP">
-      お探しのページは削除されたか、URLが変更された可能性があります。
-    </p>
+    <div v-if="error?.statusCode === 404">
+      <p class="textTitle">404 Not Found</p>
+      <p class="textH">お探しのページは見つかりませんでした。</p>
+      <p class="textP">
+        お探しのページは削除されたか、URLが変更された可能性があります。
+      </p>
+    </div>
+    <div v-else>
+      <h2 class="status">{{ error?.statusCode }}</h2>
+      <h3>{{ error?.message }}</h3>
+      <p class="link" @click="$router.go(-1);">
+        Back to Previous Page
+      </p>
+      <p class="link" @click="$router.push('/');">
+        Go to Home Page
+      </p>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const error = useError()
+</script>
