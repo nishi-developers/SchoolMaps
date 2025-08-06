e328f727ae6eb4c1cb7d1ec81696e04b48e744a0
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-06 21:54:45 +0900

    スタイルシートの追加と設定を行い、Sassを依存関係に追加

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/scripts',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+  css: ["@/assets/styles/main.scss"],
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/scripts", "@nuxt/image", "@nuxt/test-utils"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/_variables.scss";',
+        },
+      },
+    },
+  },
+});
