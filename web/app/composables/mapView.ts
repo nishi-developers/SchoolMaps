1483900fb2b1b5872129d4724c0caccf2ed7d341
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-20 23:00:21 +0900

    mapViewの途中

---
+++ web/app/composables/mapView.ts
@@ -0,0 +1,56 @@
+type Config = {};
+const defaultConfig: Config = {};
+
+type MoveStatus = {
+  position: {
+    x: number;
+    y: number;
+  };
+  zoom: number;
+  rotate: number;
+};
+
+export const useMapView = (moveStatus: Ref<MoveStatus>, config: Config = defaultConfig) => {
+  const { $map, $modes, $behaviors } = useNuxtApp();
+  let mapElement: HTMLElement | null = null;
+
+  const init = (mapWrapper: HTMLElement) => {
+    mapWrapper.appendChild($map.value);
+    mapElement = mapWrapper.children[0] as HTMLElement;
+    //
+    deleteDisabledModeObj();
+    // moveStatusの監視を開始
+    watch(
+      moveStatus,
+      () => {
+        // マップの位置を更新
+        applyMove();
+      },
+      { immediate: true, deep: true }
+    );
+  };
+
+  const applyMove = () => {
+    if (mapElement) {
+      mapElement.style.position = "absolute";
+      mapElement.style.top = `${moveStatus.value.position.y}px`;
+      mapElement.style.left = `${moveStatus.value.position.x}px`;
+      mapElement.style.transform = `rotate(${moveStatus.value.rotate}deg)`;
+      mapElement.style.transform += ` scale(${moveStatus.value.zoom})`;
+    }
+  };
+
+  const deleteDisabledModeObj = () => {
+    $modes.value.forEach((mode: Mode) => {
+      if (!mode.enable) {
+        mapElement?.querySelectorAll(`[mode="${mode.id}"]`).forEach((el: Element) => {
+          el.remove();
+        });
+      }
+    });
+  };
+
+  const applyDefaultStyle = () => {};
+
+  return { init };
+};
