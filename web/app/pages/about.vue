6410a32b909a4edb40e822878860fe56f26a89b9
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-25 01:42:31 +0900

    linterに合わせて段落冒頭の全角スペースを削除

--- web/app/pages/about.vue
+++ web/app/pages/about.vue
@@ -35,13 +35,13 @@
       </p>
       <h2>アクセス解析ツールについて</h2>
       <p>
-        　当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス(Google Analytics)」を使用しています。
+        当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス(Google Analytics)」を使用しています。
         このGoogleアナリティクスではトラフィックデータ収集のため、Cookie、広告識別子などの識別情報、データの収集に使われる類似の技術を使用しています。
         これらにより収集されたデータは匿名のものであり、個人を特定するものではありません。
         <br>
-        　なお、Cookieなどの一部の機能は、ユーザーにより無効にすることが可能です。お使いのブラウザの設定などをご確認ください。
+        なお、Cookieなどの一部の機能は、ユーザーにより無効にすることが可能です。お使いのブラウザの設定などをご確認ください。
         <br>
-        　これらのことに関して、詳しくは<a href="https://www.google.com/intl/ja/policies/privacy/partners/" target="_blank"
+        これらのことに関して、詳しくは<a href="https://www.google.com/intl/ja/policies/privacy/partners/" target="_blank"
           rel="noopener noreferrer">「ユーザーが
           Googleパートナーのサイトやアプリを使用する際の Googleによるデータ使用」</a>をご確認ください。
         Googleアナリティクスに関しては、<a href="https://marketingplatform.google.com/about/analytics/terms/jp/" target="_blank"
