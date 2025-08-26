1e4394bcfdee83769971acf4d18363167c52184d
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-27 00:40:31 +0900

    useMapEventの引数を修正し、mapStatusの型を更新。mapEventにフロアとモードのボタンデータを追加し、UIを整理。

--- web/app/composables/mapEvent.ts
+++ web/app/composables/mapEvent.ts
@@ -1,4 +1,9 @@
-export const useMapEvent = (mapStatus: Ref<MapStatus>, setPlaces: (places: Array<string | null> | null) => void) => {
+export const useMapEvent = (
+  mapStatus: ReturnType<typeof useMapStatus>,
+  setPlaces: (places: Array<string | null> | null) => void
+) => {
+  const { $modes, $floors } = useNuxtApp();
+
   const clickPlace = (event: Event) => {
     const targetPlaceId = (event.target as HTMLElement).getAttribute("place");
     setPlaces([targetPlaceId]); // 存在しないID(nullとか)が来た場合はsetPlacesで弾かれる
@@ -7,10 +12,43 @@ export const useMapEvent = (mapStatus: Ref<MapStatus>, setPlaces: (places: Array
   const setIsShowLabel = (value: boolean) => {
     isShowLabel.value = value;
   };
+  const floorsButtonData = $floors.value
+    .filter((floor) => !floor.always)
+    .map((floor) => ({
+      id: floor.id,
+      name: floor.name,
+    }));
+  const modesButtonData = $modes.value
+    .filter((mode) => !mode.always && mode.enable)
+    .map((mode) => ({
+      id: mode.id,
+      name: mode.name,
+    }));
+  const setFloor = (floorId: string | null) => {
+    if (floorId === null || mapStatus.status.value.floor === floorId) {
+      // 同じ階が選択されても何もしない
+      return;
+    }
+    mapStatus.setPlaces(null);
+    mapStatus.setFloor(floorId);
+  };
+  const setMode = (modeId: string | null) => {
+    mapStatus.setPlaces(null);
+    if (mapStatus.status.value.mode === modeId) {
+      // 同じモードが選択されたらモード解除
+      mapStatus.setMode(null);
+    } else {
+      mapStatus.setMode(modeId);
+    }
+  };
 
   return {
     clickPlace,
     isShowLabel: readonly(isShowLabel),
     setIsShowLabel,
+    floorsButtonData,
+    modesButtonData,
+    setFloor,
+    setMode,
   };
 };
