d0d5d519e9104445a88497cdaf8f27f38121cb94
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-08-26 23:59:08 +0900

    ラベル表示の制御を実装

--- web/app/composables/mapEvent.ts
+++ web/app/composables/mapEvent.ts
@@ -3,8 +3,14 @@ export const useMapEvent = (mapStatus: Ref<MapStatus>, setPlaces: (places: Array
     const targetPlaceId = (event.target as HTMLElement).getAttribute("place");
     setPlaces([targetPlaceId]); // 存在しないID(nullとか)が来た場合はsetPlacesで弾かれる
   };
+  const isShowLabel = ref(true);
+  const setIsShowLabel = (value: boolean) => {
+    isShowLabel.value = value;
+  };
 
   return {
     clickPlace,
+    isShowLabel: readonly(isShowLabel),
+    setIsShowLabel,
   };
 };
