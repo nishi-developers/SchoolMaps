381fda5f6c41108382ae179d540bb28f7b272606
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-09-02 00:06:22 +0900

    descの変換途中

--- web/app/components/MapProperty.vue
+++ web/app/components/MapProperty.vue
@@ -26,9 +26,7 @@
         <img v-for="(img, index) in place?.images" :key="index" :src="img" :alt="`${place?.name}の画像${index + 1}`"
           loading="lazy">
       </div>
-      <div id="desc">
-        {{ place?.desc }}
-      </div>
+      <div id="desc" v-html="description" />
     </div>
   </div>
 </template>
@@ -39,6 +37,7 @@ import 'viewerjs/dist/viewer.css';
 const { $modesChangeable, $floorsChangeable, $placesEnable } = useNuxtApp();
 const requestURL = useRequestURL()
 
+// propsとemitの定義
 const props = defineProps<{
   places: ReadonlyArray<string>
 }>()
@@ -46,10 +45,50 @@ const emit = defineEmits<{
   (e: 'reset-places'): void
 }>()
 
+// 場所の情報を取得
 const place = computed(() => {
   if (props.places.length !== 1) return null; // 1つだけ選択されている場合のみ表示
   return $placesEnable.value.filter(place => place.id == props.places[0])[0];
 });
+
+// 説明の初期化処理
+// jump
+// [校庭](grand)というように囲まれた部分をリンクとして扱う
+// bold
+// **bold**というように囲まれた部分を太字として扱う
+// new line
+// /nという文字列を改行として扱う
+const description = ref("");
+const jumpUrls = ref<Array<string>>([]);
+const initDesc = () => {
+  jumpUrls.value = [];
+  if (!place.value) return;
+  description.value = place.value.desc;
+  description.value = description.value.replace(/\/n/g, "<br>");
+  description.value = description.value.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>");
+  // description.value = description.value.replace(/\[(.+?)\]\((.+?)\)/g, `<a href="#${"$placesEnable".value.some(place => place.id == "$2") ? "$2" : ""}" @click.prevent="emit('reset-places'); $nextTick(() => { const el = document.getElementById('$2'); if (el) el.scrollIntoView({ behavior: 'smooth' }); })">$1</a>`);
+  description.value = description.value.replace(/\[(.+?)\]\((.+?)\)/g, (match, name, url) => {
+    jumpUrls.value.push(url);
+    return `<a href="${url}" id="jumpUrl-${jumpUrls.value.length - 1}">${name}</a>`;
+  });
+  nextTick(() => {
+    registerJumpLinks();
+  });
+};
+
+const registerJumpLinks = () => {
+  jumpUrls.value.forEach((url, index) => {
+    const el = document.getElementById(`jumpUrl-${index}`);
+    if (el) {
+      el.addEventListener('click', async (e) => {
+        e.preventDefault();
+        // これじゃあ更新されない
+        // あと外部リンクの対応
+        await navigateTo(url);
+      });
+    }
+  });
+};
 
 // viewer.jsの初期化処理
 const imageContainer = ref<HTMLElement>();
@@ -68,15 +107,22 @@ const initViewer = () => {
     }
   );
 };
+
+// ライフサイクル
 watch(place, () => {
   nextTick(() => {
+    if (!place.value) return;
+    initDesc();
     initViewer();
   });
 }, { immediate: false });
 onMounted(() => {
+  if (!place.value) return;
+  initDesc();
   initViewer();
 });
 onBeforeUnmount(() => {
+  if (!place.value) return;
   if (viewer) {
     viewer.destroy();
   }
