b711b4ac17f249781629891eef18305ced1734d5
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-09-03 21:48:50 +0900

    ESLint設定を更新し、Vueの属性改行ルールを追加。

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    rules: {
+      "vue/first-attribute-linebreak": [
+        "error",
+        {
+          singleline: "ignore",
+          multiline: "beside",
+        },
+      ],
+    },
+  }
+);
