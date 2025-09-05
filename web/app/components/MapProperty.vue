96630cbe3811e04f5b3f2d72999c793227df5482
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-09-05 23:33:40 +0900

    MapPropertyコンポーネントにviewsizeプロパティを追加し、レスポンシブデザインを改善

--- web/app/components/MapProperty.vue
+++ web/app/components/MapProperty.vue
@@ -1,34 +1,37 @@
 <template>
   <div id="property-wrapper">
-    <div v-if="place" id="property">
-      <div id="titles">
-        <div id="name">{{ place?.name }}</div>
-        <div id="share" @click="shareLink(`西高マップ @${place?.name}`, useRequestURL().href)">
-          <Icon name="share" />
-          共有
+    <div v-if="place" id="property" :class="viewsize.width < viewsize.height ? 'mobile' : 'pc'">
+      <div id="slider" />
+      <div id="property-inner">
+        <div id="titles">
+          <div id="name">{{ place?.name }}</div>
+          <div id="share" @click="shareLink(`西高マップ @${place?.name}`, useRequestURL().href)">
+            <Icon name="share" />
+            共有
+          </div>
+          <div id="closeBtn" @click='emit("reset-places")'>
+            <Icon name="cercleClose" />
+          </div>
         </div>
-        <div id="closeBtn" @click='emit("reset-places")'>
-          <Icon name="cercleClose" />
+        <div id="labels">
+          <NuxtLink v-if="$floorsChangeable.some((floor) => floor.id == place?.floor)" id="floor" class="label"
+            :to="{ name: 'search', query: { q: `floor:${place?.floor}` } }">
+            <Icon name="stairs" />
+            {{$floorsChangeable.filter((floor) => floor.id == place?.floor)[0]?.name}}
+          </NuxtLink>
+          <NuxtLink v-if="$modesChangeable.some((mode) => mode.id == place?.mode)" id="mode" class="label"
+            :to="{ name: 'search', query: { q: `mode:${place?.mode}` } }">
+            <Icon name="tag" />
+            {{$modesChangeable.filter((mode) => mode.id == place?.mode)[0]?.name}}
+          </NuxtLink>
         </div>
+        <div id="images" ref="imageContainer">
+          <img v-for="(img, index) in place?.images" :key="index" :src="img" :alt="`${place?.name}の画像${index + 1}`"
+            loading="lazy">
+        </div>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div id="desc" v-html="desc.description.value" />
       </div>
-      <div id="labels">
-        <NuxtLink v-if="$floorsChangeable.some((floor) => floor.id == place?.floor)" id="floor" class="label"
-          :to="{ name: 'search', query: { q: `floor:${place?.floor}` } }">
-          <Icon name="stairs" />
-          {{$floorsChangeable.filter((floor) => floor.id == place?.floor)[0]?.name}}
-        </NuxtLink>
-        <NuxtLink v-if="$modesChangeable.some((mode) => mode.id == place?.mode)" id="mode" class="label"
-          :to="{ name: 'search', query: { q: `mode:${place?.mode}` } }">
-          <Icon name="tag" />
-          {{$modesChangeable.filter((mode) => mode.id == place?.mode)[0]?.name}}
-        </NuxtLink>
-      </div>
-      <div id="images" ref="imageContainer">
-        <img v-for="(img, index) in place?.images" :key="index" :src="img" :alt="`${place?.name}の画像${index + 1}`"
-          loading="lazy">
-      </div>
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <div id="desc" v-html="desc.description.value" />
     </div>
   </div>
 </template>
@@ -41,6 +44,7 @@ const { $modesChangeable, $floorsChangeable, $placesEnable } = useNuxtApp();
 // propsとemitの定義
 const props = defineProps<{
   places: ReadonlyArray<string>
+  viewsize: ViewSize
 }>()
 const emit = defineEmits<(e: 'reset-places' | 'apply-url') => void>();
 
@@ -89,84 +93,139 @@ onMounted(() => {
   #property {
     touch-action: pan-x pan-y;
     user-select: text;
-    overflow: scroll;
     pointer-events: auto;
     position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 30%;
     background-color: var(--SubBaseColor);
-    padding: 20px;
+    // padding-top: 25px;
+    display: flex;
+    gap: 5px;
 
-    #titles {
-      display: flex;
-      align-items: center;
-      gap: 20px;
-      margin-bottom: 10px;
+    &.mobile {
+      width: 100%;
+      height: 30%;
+      border-radius: 20px 20px 0 0;
+      bottom: 0;
+      flex-direction: column;
 
-      #name {
-        font-size: 1.8rem;
-        font-weight: bold;
+      #slider {
+        width: 200px;
+        height: 25px;
+        margin: 0 auto;
+      }
+
+      #property-inner {
+        padding: 5px 20px 20px 20px;
+      }
+    }
+
+    &.pc {
+      width: 30%;
+      height: 100%;
+      border-radius: 0 20px 20px 0;
+      bottom: 0;
+      flex-direction: row-reverse;
+
+      #slider {
+        width: 25px;
+        height: 200px;
+        margin: auto 0;
+      }
+
+      #property-inner {
+        padding: 20px 5px 20px 20px;
+      }
+    }
+
+    #slider {
+      border-radius: 15px;
+      background-color: var(--MainColor);
+      cursor: pointer;
+
+      @media (hover: hover) {
+        &:hover {
+          background-color: var(--SubColor);
+        }
+      }
+
+      &:active {
+        background-color: var(--SubColor);
+      }
+    }
+
+    #property-inner {
+      width: 100%;
+      height: 100%;
+      overflow: scroll;
+
+      #titles {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        margin-bottom: 10px;
+
+        #name {
+          font-size: 1.8rem;
+          font-weight: bold;
+          color: var(--MainBodyColor);
+          overflow-wrap: break-word;
+        }
+
+        #share {
+          margin-left: auto;
+          font-size: 1.2rem;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          color: var(--MainBodyColor);
+        }
+
+        #closeBtn {
+          font-size: 1.5rem;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          color: var(--MainBodyColor);
+          border-radius: 50%;
+        }
+      }
+
+      #labels {
+        display: flex;
+        gap: 10px;
+        font-size: 1.2rem;
+        margin-bottom: 10px;
+
+        .label {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          color: var(--MainBodyColor);
+        }
+      }
+
+      #images {
+        display: flex;
+        overflow-x: auto;
+        scroll-snap-type: x mandatory;
+        height: 250px;
+        gap: 10px;
+        margin-bottom: 10px;
+
+        img {
+          scroll-snap-align: center;
+          height: 100%;
+          border-radius: 20px;
+          border: 2px solid var(--SubColor);
+          cursor: zoom-in;
+        }
+      }
+
+      #desc {
+        font-size: 1.1rem;
         color: var(--MainBodyColor);
         overflow-wrap: break-word;
       }
-
-      #share {
-        margin-left: auto;
-        font-size: 1.2rem;
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        color: var(--MainBodyColor);
-      }
-
-      #closeBtn {
-        font-size: 1.5rem;
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        color: var(--MainBodyColor);
-        border-radius: 50%;
-      }
-    }
-
-    #labels {
-      display: flex;
-      gap: 10px;
-      font-size: 1.2rem;
-      margin-bottom: 10px;
-
-      .label {
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        color: var(--MainBodyColor);
-      }
-    }
-
-    #images {
-      display: flex;
-      overflow-x: auto;
-      scroll-snap-type: x mandatory;
-      height: 250px;
-      gap: 10px;
-      margin-bottom: 10px;
-
-      img {
-        scroll-snap-align: center;
-        height: 100%;
-        border-radius: 20px;
-        border: 2px solid var(--SubColor);
-        cursor: zoom-in;
-      }
-    }
-
-    #desc {
-      font-size: 1.1rem;
-      color: var(--MainBodyColor);
-      overflow-wrap: break-word;
     }
   }
-
 }
 </style>
