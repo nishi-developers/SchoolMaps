3f4cbc691b3df2559ae4365b838cc250bce84a84
M_Haruki <105488840+M-Haruki@users.noreply.github.com>
2025-09-13 00:37:53 +0900

    エラーページのデザインを改善し、アクションボタンを追加 アイコンをnuxt.config.tsに追加

--- web/app/error.vue
+++ web/app/error.vue
@@ -1,27 +1,91 @@
 <template>
   <div class="page">
-    <div v-if="error?.statusCode === 404">
+    <div v-if="error?.statusCode === 404" class="info">
       <h1>404 Not Found</h1>
       <h2>お探しのページは見つかりませんでした。</h2>
-      <p>
+      <p class="message">
         お探しのページは削除されたか、URLが変更された可能性があります。
       </p>
     </div>
-    <div v-else>
-      <h1 class="status">{{ error?.statusCode }}</h1>
-      <h2>{{ error?.message }}</h2>
-      <p class="link" @click="$router.back();">
-        Back to Previous Page
-      </p>
-      <p class="link" @click="navigateTo('/');">
-        Go to Home Page
-      </p>
+    <div v-else class="info">
+      <h1>
+        {{ error?.statusCode + ' ' + (error?.statusMessage || 'エラー') }}
+      </h1>
+      <h2>予期しないエラーが発生しました。</h2>
+      <p class="message">{{ error?.message || 'エラーが発生しました。' }}</p>
+    </div>
+    <div class="actions">
+      <div class="action" @click="back">
+        <Icon name="back" />
+        <span>元のページに戻る</span>
+      </div>
+      <NuxtLink class="action" to="/">
+        <Icon name="home" />
+        <span>トップに戻る</span>
+      </NuxtLink>
+      <div class="action" @click="reload">
+        <Icon name="reload" />
+        <span>再読み込み</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const error = useError()
+import { NuxtLink } from '#components';
 
-useHead({ title: error.value?.statusCode })
+const error = useError()
+const router = useRouter()
+
+useHead({
+  title: error.value?.statusCode,
+  meta: [
+    { name: 'robots', content: 'noindex, nofollow' }
+  ]
+})
+
+function back() {
+  router.back()
+}
+
+function reload() {
+  window.location.reload();
+}
 </script>
+<style scoped lang="scss">
+.info {
+  .message {
+    font-size: 1.2rem;
+    color: var(--TextSecondaryColor);
+    margin-top: 10px;
+  }
+}
+
+.actions {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 1.25rem;
+
+  .action {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: var(--LinkBodyColor);
+    gap: 3px;
+    text-decoration: none;
+
+    @media (hover: hover) {
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    &:active {
+      text-decoration: underline;
+    }
+  }
+
+}
+</style>
